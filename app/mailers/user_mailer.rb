6b8d39060bef77b6ad28a267348f70308b90afc0
Rachel Wong <rachelwong.ly@gmail.com>
2019-02-25 22:07:17 +0000

    implemented order receipt functionality

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
- 
-  def order_confirmation_email(order)
+
+  def order_receipt_email(order)
     @order = order
     @url  = 'http://localhost:3000/orders/#{order.id}'
     mail(to: @order.email, subject: 'Order Confirmation for order #{order.id}')
